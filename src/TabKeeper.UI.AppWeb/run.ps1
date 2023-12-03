2fb96abd868c4709a38d6b7a458b9ff921ecd3a8
Panos <panos_ath@outlook.com>
2023-12-03 17:16:29 +0200

    refactor: run.ps1

--- src/TabKeeper.UI.AppWeb/run.ps1
+++ src/TabKeeper.UI.AppWeb/run.ps1
@@ -3,30 +3,39 @@ param (
     [switch] $css = $false,
     [switch] $css_watch = $false,
     [switch] $dotnet_watch = $false,
-    [switch] $watch = $false
+    [switch] $watch = $false,
+    [switch] $publish = $false
 )
 
+# CSS
 if ($css) {
     tailwindcss -i .\assets\app.scss -o .\wwwroot\css\app.css
 
     exit
 }
-
 if ($css_watch) {
     tailwindcss -i .\assets\app.scss -o .\wwwroot\css\app.css --watch
 
     exit
 }
 
+# .NET
 if ($dotnet_watch) {
     dotnet watch run
 
     exit
 }
 
+# Common
 if ($watch) {
     Start-Job { tailwindcss -i .\assets\app.scss -o .\wwwroot\css\app.css --watch }
     dotnet watch run
+
+    exit
+}
+if ($publish) {
+    tailwindcss -i .\assets\app.scss -o .\wwwroot\css\app.css --minify
+    dotnet publish
 
     exit
 }
