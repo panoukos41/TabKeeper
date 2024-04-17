84255f633d0b0922e7d7b334097eb08fd9afd0ac
Panos <panos_ath@outlook.com>
2024-04-17 21:18:40 +0300

    minor

--- src/TabKeeper.UI.Wasm/css.ps1
+++ src/TabKeeper.UI.Wasm/css.ps1
@@ -4,12 +4,14 @@ param (
     [switch] $publish = $false
 )
 
-$i = ".\assets\app.scss"
-$o = ".\wwwroot\css\app.min.css"
+$i = "./assets/app.scss"
+$o = "./wwwroot/css/app.min.css"
 $cmd = "tailwindcss", "-i $i", "-o $o", "--postcss"
 
 if ($watch) { $cmd += "--watch" }
 elseif ($publish) { $cmd += "--minify" }
 
-Write-Host ($cmd | Join-String -Separator ' ')
-Invoke-Expression -Command ($cmd | Join-String -Separator ' ')
+$cmd = $cmd | Join-String -Separator ' '
+
+Write-Host $cmd
+Invoke-Expression -Command $cmd
