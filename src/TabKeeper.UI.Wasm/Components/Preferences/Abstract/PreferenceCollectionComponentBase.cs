26275e816f91b4ee6e0578c483b086b39192e367
Panos <panos_ath@outlook.com>
2024-11-03 17:58:31 +0200

    refactor(settings): Implement new preferences API

---
+++ src/TabKeeper.UI.Wasm/Components/Preferences/Abstract/PreferenceCollectionComponentBase.cs
@@ -0,0 +1,33 @@
+﻿using Core.Preferences;
+using Core.Preferences.Abstract;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using TabKeeper.Components.Preferences.Components;
+
+namespace TabKeeper.Components.Preferences.Abstract;
+
+public abstract class PreferenceCollectionComponentBase<TPreferenceCollection> : PreferenceComponentBase<TPreferenceCollection>
+    where TPreferenceCollection : PreferenceCollectionBase
+{
+    protected RenderFragment RenderPreference(PreferenceBase preference) => builder =>
+    {
+        _ = preference switch
+        {
+            PreferenceCategory categoryPreference => Render<PreferenceCategoryComponent, PreferenceCategory>(ref builder, ref categoryPreference),
+            EditTextPreference editTextPreference => Render<EditTextPreferenceComponent, EditTextPreference>(ref builder, ref editTextPreference),
+            SwitchPreference switchPreference => Render<SwitchPreferenceComponent, SwitchPreference>(ref builder, ref switchPreference),
+            ListBoxPreference listBoxPreference => Render<ListBoxPreferenceComponent, ListBoxPreference>(ref builder, ref listBoxPreference),
+            _ => Nothing.Value
+        };
+    };
+
+    private static Nothing Render<TComponent, TPreference>(ref RenderTreeBuilder builder, ref TPreference preference)
+        where TComponent : notnull, PreferenceComponentBase<TPreference>
+        where TPreference : PreferenceBase
+    {
+        builder.OpenComponent<TComponent>(0);
+        builder.AddComponentParameter(1, nameof(Preference), preference);
+        builder.CloseComponent();
+        return Nothing.Value;
+    }
+}
---
+++ src/TabKeeper.UI/Preferences.cs
@@ -0,0 +1,37 @@
+﻿using Core.Preferences;
+using Core.Preferences.Builders;
+
+namespace TabKeeper;
+
+public static class Preferences
+{
+    public const string Language = "lang";
+
+    public const string Theme = "theme";
+
+    public static PreferenceScreen Screen { get; } = PreferenceScreenBuilder
+        .CreateEmpty()
+        .AddCategory(b => b
+            .WithTitle("settings.lang.title")
+            .AddListBox(new()
+            {
+                Key = Preferences.Language,
+                Title = "settings.lang.title",
+                AllowedValues = ["en", "el"],
+                DefaultValue = "en",
+                SummaryProvider = value => $"settings.lang.{value}",
+            })
+        )
+        .AddCategory(b => b
+            .WithTitle("settings.theme.title")
+            .AddListBox(new()
+            {
+                Key = Preferences.Theme,
+                Title = "settings.theme.title",
+                AllowedValues = ["auto", "dark", "light"],
+                DefaultValue = "auto",
+                SummaryProvider = value => $"settings.theme.{value}",
+            })
+        )
+    .Build();
+}
