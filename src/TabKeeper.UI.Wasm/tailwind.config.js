ebb26b026b4d151d76498eb6dfda4c28a358b0bc
Panos <panos_ath@outlook.com>
2024-11-03 17:56:43 +0200

    refactor: tailwind config

--- src/TabKeeper.UI.Wasm/tailwind.config.js
+++ src/TabKeeper.UI.Wasm/tailwind.config.js
@@ -19,14 +19,17 @@ module.exports = withMaterialColors(
     }
   },
   {
-    primary: "#0035BE",
-    secondary: "#005A6F",
-    tertiary: "#054C6C",
-    error: "#D4352F",
-    neutral: "#919094"
+    primary: "#E6E6FA",
+    //secondary: "#005A6F",
+    //tertiary: "#054C6C",
+    //error: "#D4352F",
+    //neutral: "#919094"
   },
   {
-    scheme: "content",
+    /* one of 'content', 'expressive', 'fidelity', 'monochrome', 'neutral', 'tonalSpot' or 'vibrant' */
+    scheme: "expressive",
+    // contrast is optional and ranges from -1 (less contrast) to 1 (more contrast).
+    contrast: 0,
     extend: true
   }
 );
