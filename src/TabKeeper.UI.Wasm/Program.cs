bccab2cdc536cf70cb75e70c720b777751e3e405
Panos <panos_ath@outlook.com>
2025-03-31 23:11:39 +0300

    Add missing translations and remove dialog outlet

--- src/TabKeeper.UI.Wasm/Program.cs
+++ src/TabKeeper.UI.Wasm/Program.cs
@@ -1,6 +1,5 @@
 using Blazored.LocalStorage;
 using Core.Preferences;
-using Ignis.Components.HeadlessUI;
 using Ignis.Components.WebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -16,7 +15,6 @@ var configuration = builder.Configuration;
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.RootComponents.Add<DialogOutlet>("#dialog-outlet");
 
 services.AddIgnisWebAssembly();
 services.AddBlazoredLocalStorage(c => c.JsonSerializerOptions = Options.Json);
